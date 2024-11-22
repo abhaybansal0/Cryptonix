912519f8907f45c4cd04afa7e6bab86cde64af6c
Abhay Bansal <27abhay.bansal@gmail.com>
2024-11-22 20:26:47 +0530

    Incomplete Google Auth

--- app/login/page.js
+++ app/login/page.js
@@ -1,26 +1,37 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
 import './page.css'
+import { signIn } from 'next-auth/react';
+
 
 const Login = () => {
+
+    const handleLogin = () => {
+        signIn('google'); // This will trigger Google authentication
+    };
+
+
     return (
-            <div className='flex flex-col justify-center items-center gap-12
+        <div className='flex flex-col justify-center items-center gap-12
                  w-screen h-screen bg-white text-black text-4xl
                  text-center'>
-                <div>
+            <div>
 
-                    Please Log in With Your
-                    <span className='max-w-fit mx-3 hilightgoogel'>
-                        Google
-                    </span>
-                    Account.
+                Please Log in With Your
+                <span className='max-w-fit mx-3 hilightgoogel'>
+                    Google
+                </span>
+                Account.
 
-                </div>
-                <Link href="/">
-                <button className='bg-black text-white w-fit py-4 px-8 border rounded-full '>Log In</button>
-                </Link>
             </div>
+            <button className='bg-black text-white w-fit py-4 px-8 border rounded-full '
+                onClick={handleLogin}
+            >
+                Log In
+            </button>
+        </div>
     )
 }
 
-export default Login
+export default Login;
