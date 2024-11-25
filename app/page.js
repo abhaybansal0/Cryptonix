1bc803b11abc6507b1632da79f2a47f02d800e80
Abhay Bansal <27abhay.bansal@gmail.com>
2024-11-25 06:32:18 +0530

    Home

--- app/page.js
+++ app/page.js
@@ -6,14 +6,10 @@ import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession()
-  if(session) {
-    return <>
-      Signed in as {session.user.email} <br/>
-      <button onClick={() => signOut()}>Sign out</button>
-    </>
-  }
-  return <>
-    Not signed in <br/>
-    <button onClick={() => signIn()}>Sign in</button>
-  </>
+  return (
+
+    <div className="flex items-center justify-center">
+      <img src="./protection.png" alt="HOME" className="w-52" />
+    </div>
+  )
 }
